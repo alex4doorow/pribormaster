ad259532dbb6e2a0e772230bb636e3707e98474e
alex4doorow <alex4doorow@gmail.com>
2023-04-14 14:56:08 +0300

    #101 orderService.add: squash 1

--- core/src/test/java/com/sir/richard/boss/services/OrderServiceTest.java
+++ core/src/test/java/com/sir/richard/boss/services/OrderServiceTest.java
@@ -36,6 +36,7 @@ public class OrderServiceTest {
 
     @Test
     public void testCustomerOrderFindById() {
+        log.info("squash 1");
         Order order = orderService.findById(10714L);
         log.info("order: {}", order);
         Assertions.assertNotNull(order.getCustomer());
