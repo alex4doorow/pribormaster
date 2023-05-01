1a80dfd477aaa62514d3a5e53c25077044a4d081
alex4doorow <alex4doorow@gmail.com>
2023-05-01 18:13:59 +0300

    #004: WEB. Resolve exception page

--- shop-rest/src/main/java/com/sir/richard/boss/StartRestApplication.java
+++ shop-rest/src/main/java/com/sir/richard/boss/StartRestApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartRestApplication.class, args);
-
     }
 }
