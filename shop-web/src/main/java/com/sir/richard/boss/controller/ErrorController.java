9ce8108bf453f9012c0e27b1905fe42ec0f274d8
alex4doorow <alex4doorow@gmail.com>
2023-05-02 16:39:18 +0300

    #004: WEB. Spring security: ErrorController

---
+++ shop-web/src/main/java/com/sir/richard/boss/controller/ErrorController.java
@@ -0,0 +1,22 @@
+package com.sir.richard.boss.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(final Throwable throwable, final Model model) {
+        log.error("Exception during execution of SpringSecurity application", throwable);
+        String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        model.addAttribute("message", errorMessage);
+        return "error";
+    }
+}
